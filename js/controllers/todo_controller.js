816bfee5a40fd4f5be6e7cd27e125efcc8dd718b
Philippe Schwyter <pschwyter90@gmail.com>
2015-01-26 20:26:22 -0500

    Added ability to delete a model

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -3,6 +3,33 @@ Todos.TodoController = Ember.ObjectController.extend({
     editTodo: function () {
       this.set('isEditing', true);
     },
+
+    doneEditing: function () {
+      var bufferedTitle = this.get('bufferedTitle').trim();
+
+      if (Ember.isEmpty(bufferedTitle)) {
+        // The `doneEditing` action gets sent twice when the user hits
+        // enter (once via 'insert-newline' and once via 'focus-out').
+        //
+        // We debounce our call to 'removeTodo' so that it only gets
+        // made once.
+        Ember.run.debounce(this, 'removeTodo', 0);
+      } else {
+        var todo = this.get('model');
+        todo.set('title', bufferedTitle);
+        todo.save();
+      }
+
+      // Re-set our newly edited title to persist its trimmed version
+      this.set('bufferedTitle', bufferedTitle);
+      this.set('isEditing', false);
+    },
+
+    cancelEditing: function () {
+      this.set('bufferedTitle', this.get('title'));
+      this.set('isEditing', false);
+    },
+    
     acceptChanges: function() {
       this.set('isEditing', false);
 
