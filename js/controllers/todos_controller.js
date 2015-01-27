e014148044733f81bb92524c46af28a44786d8dc
Philippe Schwyter <pschwyter90@gmail.com>
2015-01-26 21:36:41 -0500

    Added link and controller property to indicate all todos are complete

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -40,8 +40,11 @@ Todos.TodosController = Ember.ArrayController.extend({
 
   completed: function() {
     return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  allAreDone: function(key, value) {
+    return !!this.get('length') && this.isEvery('isCompleted');
   }.property('@each.isCompleted')
-  
 });
 
 
